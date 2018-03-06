733ffd0729cc9301391457bd6efc57d48b396cdd
marwenchb <marwenchb@gmail.com>
2018-03-06 17:06:03 +0100

    items controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #get 'items/index'
+
+  #get 'items/show'
+
+  root 'items#index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
