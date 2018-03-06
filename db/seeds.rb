3feb1a2184778a5b65e1c2058989ddd8d1c14faa
FabienMllts <fabienmallet@gmail.com>
2018-03-06 10:54:54 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 test = test.create([{name: 'test'}])
+
+ Item.create(title: 'poire', description: 'fruity fruity', price: 10, img_url: "http://www.olivepressee.com/31-large_default/vinaigre-balsamique-poire.jpg")
+ Item.create(title: 'pomme', description: 'fruity fruity', price: 11, img_url: "http://images.all-free-download.com/images/graphicthumb/red_and_green_apple_310425.jpg")
+ Item.create(title: 'abricot', description: 'fruity fruity', price: 12, img_url: "https://www.aroma-zone.com/cms/sites/default/files/fiche_technique/fragrances_cosmetiques/Visuel_sorbet-abricot.png")
+ Item.create(title: 'pêche', description: 'fruity fruity', price: 13, img_url: "http://img-3.journaldesfemmes.com/IrV3p2mnAxSul6Oj1jjsk1Xofxk=/910x607/smart/23f0e9d66c2745c8b36b1a4c3c79d1a2/ccmcms-jdf/10662450.jpg")
+ Item.create(title: 'banane', description: 'fruity fruity', price: 14, img_url: "http://mamul.am/images/photos/150322/banann-azatum-e-stre-n64757-1.jpg")
+ 
